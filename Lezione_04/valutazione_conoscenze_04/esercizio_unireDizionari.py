c85f8774a552513e281e8bea4cf18638dae4f555
Matthew-2002 <matteofabbri3003@gmail.com>
2025-03-20 14:45:12 +0100

    ultimo es test

--- Lezione_04/valutazione_conoscenze_04/esercizio_unireDizionari.py
+++ Lezione_04/valutazione_conoscenze_04/esercizio_unireDizionari.py
@@ -7,11 +7,11 @@ def merge_dictionaries(dict1: dict, dict2: dict) -> dict:
             else:
                 new_dict[key] = dict1[key]
     for key, value in dict2.items():
-        for key2, value2 in new_dict.items():
+        for key2, value2 in dict1.items():
             if key != key2:
                 new_dict[key] = dict2[key]
 
     return new_dict
 
 
-print(merge_dictionaries({'x': 5}, {'x': -5}))
+print(merge_dictionaries({'a': 1, 'b': 2}, {'b': 3, 'c': 4}))
