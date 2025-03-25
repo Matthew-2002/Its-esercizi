263d4b29d82669196a49a8e2793f277d9386a8f7
Matthew-2002 <matteofabbri3003@gmail.com>
2025-03-25 19:51:46 +0100

    traccia opz 2 generare numeri casuali

---
+++ Lezione_04/opzionali/opz_4_02.py
@@ -0,0 +1,12 @@
+'''2. Guess the Number Game:
+
+    Create a function that generates a random number 
+    within a range specified by the user.
+    Prompt the user to guess the number within a specified 
+    maximum number of attempts.
+    Provide feedback to the user after each guess, indicating 
+    whether their guess is too high, too low, or correct.
+    Terminate the loop when the user guesses the number 
+    correctly or reaches the maximum number of attempts.
+'''
+
