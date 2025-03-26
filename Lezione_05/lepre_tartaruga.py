09ec7b5e14bd2e8c162efe2e031b3d7876fb4b6b
Matthew-2002 <matteofabbri3003@gmail.com>
2025-03-26 18:56:37 +0100

    correzioni lepre tartaruga

--- Lezione_05/lepre_tartaruga.py
+++ Lezione_05/lepre_tartaruga.py
@@ -8,80 +8,86 @@ def posizione_gara(posizione_t, posizione_l):
     percorso: list[any] = []
     for i in range(1, 73):
         percorso.append("_")    
-    if posizione_t == posizione_l:
-        print("OUCH!!!")
-        percorso[posizione_t] = "T"
-        percorso[posizione_l-1] =  "L"
-    else:
-        percorso.insert(posizione_t, "T")
-        percorso.insert(posizione_l, "L")
-    print("".join(percorso))
+    if posizione_t-1 >= len(percorso):
+        posizione_t = len(percorso)
+    elif posizione_l-1 >= len(percorso):
+        posizione_l = len(percorso)
+    else:    
+        if posizione_t == posizione_l:
+            print("OUCH!!!")
+            percorso[posizione_t] = "T"
+            percorso[posizione_l-1] =  "L"
+        else:
+            percorso[posizione_t-1] =  "T"
+            percorso[posizione_l-1] =  "L"
+        print("".join(percorso))
 
 def mossa_t(n, posizione_t, meteo):
     if meteo == 'soleggiato':   
-        if 1 <= n <= 5:
+        if 1 <= n <= 50:
             posizione_t += 3
-        elif n == 6 or n == 7:
+        elif 51 <= n <= 80:
+            posizione_t += 1
+        else:
             posizione_t -= 6
             if posizione_t < 1:
                 posizione_t = 1
-        else:
-            posizione_t += 1
         return posizione_t
     if meteo == 'piovoso':
-        if 1 <= n <= 5:
+        if 1 <= n <= 45:
             posizione_t += 3
-        elif 6 <= n <= 10:
+        elif 46 <= n <= 70:
+            posizione_t += 1
+        else:
             posizione_t -= 6
+        posizione_t -= 1
         if posizione_t < 1:
             posizione_t = 1
-        else:
-            posizione_t += 1
         return posizione_t
 
 def mossa_l(n, posizione_l, meteo):
     if meteo == 'soleggiato':
-        if n == 1 or n == 2:
+        if 1 <= n <= 20:
             posizione_l += 9
-        elif n == 3:
-            posizione_l -= 12
-            if posizione_l < 1:
-                posizione_l = 1
-        elif 4 <= n <= 6:
+        elif 21 <= n <= 50:
             posizione_l += 1
-        elif n == 7 or n == 8:
+        elif 51 <= n <= 70:
             posizione_l -= 2
-            if posizione_l < 1:
-                posizione_l = 1
+        elif 71 <= n <= 80:
+            posizione_l -= 12        
+        if posizione_l < 1:
+            posizione_l = 1
         return posizione_l
     if meteo == 'piovoso':
-        if n == 1 or n == 2:
+        if 1 <= n <= 17:
             posizione_l += 9
-        elif n == 3 or n == 10:
-            posizione_l -= 12
-            if posizione_l < 1:
-                posizione_l = 1
-        elif 4 <= n <= 6:
+        elif 18 <= n <= 44:
             posizione_l += 1
-        elif 7 <= n <= 9:
+        elif 45 <= n <= 70:
             posizione_l -= 2
-            if posizione_l < 1:
-                posizione_l = 1
+        elif 71 <= n <= 83:
+            posizione_l -= 12        
+        posizione_l -= 2
+        if posizione_l < 1:
+            posizione_l = 1
         return posizione_l
+
 print("BANG !!!!! AND THEY'RE OFF !!!!!")
 posizione_gara(posizione_t, posizione_l)
+
 i: int = 0
 while posizione_l < 71 and posizione_t < 71:
+    
     if (i // 10) % 2 == 0:
         meteo: str = 'soleggiato'
     else:
         meteo: str = 'piovoso'
-    posizione_l = mossa_l((random.randint(1, 12)), posizione_l, meteo)
-    posizione_t = mossa_t((random.randint(1, 12)), posizione_t, meteo)
+    posizione_l = mossa_l((random.randint(1, 100)), posizione_l, meteo)
+    posizione_t = mossa_t((random.randint(1, 100)), posizione_t, meteo)
     posizione_gara(posizione_t, posizione_l)
     i += 1
 
-if posizione_t > 70 and posizione_l > 70:
+if posizione_t > 72 and posizione_l > 72:
     print("TORTOISE WINS! || VAY!!!")
 elif posizione_t > 70:
     print("TORTOISE WINS! || VAY!!!")
