406ca639e4ca55c36895357723a2cfb932713564
Matthew-2002 <matteofabbri3003@gmail.com>
2025-09-26 18:29:48 +0200

    varie

--- Python/Lezione_01/Hello.py
+++ Python/Lezione_01/Hello.py
@@ -1,25 +1,33 @@
-print ("Hello World!")
-print ("hey there")
+#print ("Hello World!")
+#print ("hey there")
 
-from datetime import date
+from datetime import datetime, timedelta
 
 assenze: float = 187
-erogate: float = 605
+erogate: float = 632.5
 presenze: float = erogate - assenze
 
-#dati aggiornati al 31 luglio
+#dati aggiornati al 26 settembre
 
-obiettivo: float = 0.2
-
+max_assenze: float = 0.2
 giorni: int = 0
+giorni_lez_mancanti = (1080 - erogate) // 5
+giorni_tot_mancanti = giorni_lez_mancanti+giorni_lez_mancanti*2/7
 
-print(f'{(assenze*100/erogate):.2f}%')
+print(f'Percentuale asssenza attuale {(assenze*100/erogate):.2f}%')
 
-while (assenze/erogate) > obiettivo:
+while (assenze/erogate) > max_assenze:
     erogate += 5
     giorni += 1
 
-print(giorni)
+giorni_per_obiettivo = giorni*2/7 + giorni
+data_obiettivo = datetime.today() + timedelta(giorni_per_obiettivo)
+data_fine_corso = datetime.today() + timedelta(giorni_tot_mancanti)
+
+print(f'Previsione raggiungimento obiettivo: ~ {data_obiettivo.strftime("%d-%m-%Y")}\n\
+Giorni di lezione all\'obiettivo: ~ {giorni}\nPrevisione termine corso: ~ \
+{data_fine_corso.strftime("%d-%m-%Y")}\nGiorni al termine del corso: ~ {giorni_tot_mancanti:.0f}\n\
+Giorni al termine dell\'obiettivo: ~ {giorni_per_obiettivo:.0f}')
 
     
 
---
+++ Python/Lezione_01/prove.py
@@ -0,0 +1,60 @@
+def dedup_stable(nums: list[int]) -> list[int]:
+    new_nums = []
+    for num in nums:
+        lunghezza = len(str(num))
+        new_num = num//(10**(lunghezza-1))
+        if new_nums: 
+            if new_num != new_nums[-1]:
+                new_nums.append(new_num)
+        else:
+            new_nums.append(new_num)
+    return new_nums
+
+
+def chunk(lst: list[int], size: int) -> list[list[int]]:
+    new_list = []
+    if not lst:
+        return new_list
+    i = len(lst)//size
+    index = 0
+    while index < len(lst):
+        if i == 0:
+            size = len(lst)%size 
+        for j in range(size):
+            if j == 0:
+                temp_list = [lst[index]]
+            else:
+                temp_list.append(lst[index])
+            index += 1
+        new_list.append(temp_list)
+        i -= 1
+    return new_list
+
+            
+def flatten_once(nested: list[list[int]]) -> list[int]:
+    new_list = []
+    for list in nested:
+        for num in list:
+            new_list.append(num)
+    return new_list
+
+
+def letter_count(text: str) -> dict[str,int]:
+    occorrenze = {}
+    for l in text:
+        if l.isalpha():
+                l = l.lower()
+                if l in occorrenze:
+                    occorrenze[l] += 1
+                else:
+                    occorrenze[l] = 1
+    return occorrenze
+
+
+def deep_get(d, path: list, default=None):
+    try:
+        for item in path:
+            d = d[item]
+        return d
+    except:
+        return default
